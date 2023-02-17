9fe5cf59948fef4c15b6c2bb5f0d3b833a28331f
Andrey Sushkov <>
2023-02-17 17:09:10 +0300

    Initial commit

---
+++ MVVM sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM sample
+//
+//  Created by Andrei Sushkou on 17.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
