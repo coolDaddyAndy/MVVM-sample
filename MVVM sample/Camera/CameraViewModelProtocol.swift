d3455bf8e63627b19de742b2500aeea748076fa8
Andrey Sushkov <>
2023-02-17 17:32:25 +0300

    ViewModel protocol and model for modes added

---
+++ MVVM sample/Camera/CameraViewModelProtocol.swift
@@ -0,0 +1,20 @@
+//
+//  CameraViewModelProtocol.swift
+//  MVVM sample
+//
+//  Created by Andrei Sushkou on 17.02.23.
+//
+
+import UIKit
+
+
+protocol CameraViewModelProtocol {
+    var isFlashEnabled:        Bool { get set }
+    var isLivePhotoEnabled:    Bool { get set }
+    var isWideScreenAvailable: Bool { get }
+    var isWideScreenEnabled:   Bool { get set }
+    var isCameraFlipAvailable: Bool { get }
+    
+    func getAvailableCameraModes() -> [CameraMode]
+    func getRecentImage() -> UIImage?
+}
---
+++ MVVM sample/Models/CameraMode.swift
@@ -0,0 +1,15 @@
+//
+//  CameraMode.swift
+//  MVVM sample
+//
+//  Created by Andrei Sushkou on 17.02.23.
+//
+
+
+enum CameraMode {
+    case photo
+    case portrait
+    case video
+    case slowMo
+    case pano
+}
